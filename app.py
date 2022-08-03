1fb9edf82b96b1e7a24fad2044c4d89977694b2e
Vitnelkad <aumdrop@gmail.com>
2022-08-03 22:49:51 +0300

    initial

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
-from flask import Flask
+import flask
+from flask import Flask, jsonify
+from utils import *
 
 app = Flask(__name__)
+
+app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/movie/<title>')
+def get_by_title(title):
+    return movie_by_title(title)
+
+@app.route('/movie/<int:year1>/to/<int:year2>')
+def get_movies_by_years(year1, year2):
+    return flask.jsonify(movies_by_years(year1, year2))
+
+@app.route('/rating/<category>')
+def get_movies_by_rating(category):
+    return flask.jsonify(movies_by_rating(category))
+
+
+@app.route('/genre/<genre>')
+def get_movies_by_genre(genre):
+    return flask.jsonify(movies_by_genre(genre))
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+# type — фильм или сериал
+# title — название
+# director — режиссер
+# cast — основные актеры
+# country — страна производства
+# date_added — когда добавлен на Нетфликс
+# release_year — когда выпущен в прокат
+# rating — возрастной рейтинг
+# duration — длительность
+# duration_type — минуты или сезоны
+# listed_in — список жанров и подборок
+# description — краткое описание
+
+import collections
 import sqlite3
 
 
@@ -10,7 +24,15 @@ class DbConnect:
         self.cur.close()
         self.con.close()
 
-def get_by_title(title):
+def execute_query(query):
+    with sqlite3.connect('netflix.db') as con:
+        cur = con.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+    return result
+
+
+def movie_by_title(title):
     db_connect = DbConnect('netflix.db')
     db_connect.cur.execute(
         f"""SELECT title, country, release_year, listed_in, description
@@ -27,4 +49,77 @@ def get_by_title(title):
         "description": result[4],
     }
 
-print(get_by_title('100'))
+
+def movies_by_years(year1, year2):
+    db_connect = DbConnect('netflix.db')
+    query = f"select title, release_year from netflix where release_year between {year1} and {year2} limit 100;"
+    db_connect.cur.execute(query)
+    result = db_connect.cur.fetchall()
+    result_list = []
+    for movie in result:
+        result_list.append({"title": movie[0],
+                            "release_year": movie[1]})
+    return result_list
+
+def movies_by_rating(rating):
+    rating_parameters = {
+        "children": "'G'",
+        "family": "'G', 'PG', 'PG-13'",
+        "adult": "'R', 'NC-17'"
+    }
+    if rating not in rating_parameters:
+        return "Переданной группы не существует"
+    query = f"select title, rating, description from netflix where rating in ({rating_parameters[rating]})"
+    result = execute_query(query)
+    result_list = []
+    for movie in result:
+        result_list.append({
+            "title": movie[0],
+            "rating": movie[1],
+            "description": movie[2]
+        })
+    return result_list
+
+def movies_by_genre(genre):
+    result = execute_query(f""" select title, description
+    from netflix
+    where listed_in like '%{genre}%'
+    order by  release_year desc
+    limit 10;""")
+    result_list = []
+    for movie in result:
+        result_list.append({
+            "title": movie[0],
+            "description": movie[1]
+        })
+        return result_list
+
+
+def cast_parttners(actor1, actor2):
+    query = f"select 'cast' from netflix where 'cast' like '%{actor1}%' and 'cast' like '%{actor2}%';"
+    result = execute_query(query)
+    actors_list = []
+    for cast in result:
+        actors_list.extend(cast[0].split(', '))
+    counter = collections.Counter(actors_list)
+    result_list = []
+    for actor, count in counter.items():
+        if actor not in [actor1, actor2] and count > 2:
+            result_list.append(actor)
+    return result_list
+
+def search_movie_by_param(movie_type, release_year, genre):
+    query = f"""select title, description
+    from netflix
+    where type = '{movie_type}' 
+    and release_year = {release_year}
+    and listed_in like '%{genre}%'"""
+    result = execute_query(query)
+    result_list = []
+    for movie in result:
+        result_list.append({'title': movie[0],
+                           'description': movie[1]})
+    return result_list
+
+print(search_movie_by_param('TV Show', 2005, 'Drama'))
+    
